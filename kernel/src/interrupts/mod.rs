b0bc6a3b0a37eec73a6f7940c8a772e95707af18
BigBadE <bigbade333@gmail.com>
2022-11-28 22:05:51 -0600

    Multiple bug fixes

--- kernel/src/interrupts/mod.rs
+++ kernel/src/interrupts/mod.rs
@@ -1,15 +1,18 @@
+use crate::println;
+
 pub mod interrupts;
 pub mod pic8259_interrupts;
 pub mod gdt;
 
 pub fn init() {
+    println!("Loading interrupts");
     // Init the GDT first, then CPU interrupts
     gdt::init();
     interrupts::init_idt();
 
     // Init hardware interrupts last
-    //unsafe { pic8259_interrupts::PICS.lock().initialize() };
+    unsafe { pic8259_interrupts::PICS.lock().initialize() };
 
     // Enable interrupt controller
-    //x86_64::instructions::interrupts::enable();
+    x86_64::instructions::interrupts::enable();
 }
